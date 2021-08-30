55a3f1916cb279d264fc77617711046a477771df
BayuPrasetya <bayud40@gmail.com>
2021-08-30 16:14:42 +0700

    Date picker

--- app/src/main/java/com/ikm/inventoryqrscanner/activity/CreateActivity.kt
+++ app/src/main/java/com/ikm/inventoryqrscanner/activity/CreateActivity.kt
@@ -1,12 +1,33 @@
 package com.ikm.inventoryqrscanner.activity
 
 import android.os.Bundle
+import android.util.Log
 import com.ikm.inventoryqrscanner.BaseActivity
-import com.ikm.inventoryqrscanner.R
+import com.ikm.inventoryqrscanner.databinding.ActivityCreateBinding
+import com.ikm.inventoryqrscanner.fragment.DateFragment
 
 class CreateActivity : BaseActivity() {
+
+    private val binding by lazy { ActivityCreateBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_create)
+        setContentView(binding.root)
+        setupListener()
     }
+
+    private fun setupListener() {
+
+        binding.editDate.setOnClickListener {
+            DateFragment(object : DateFragment.DateListener {
+                override fun onSuccess(date: String) {
+                    binding.editDate.setText(date)
+                }
+
+            }).apply { show(supportFragmentManager, "dateFragment")}
+        }
+
+    }
+
+
 }
---
+++ app/src/main/java/com/ikm/inventoryqrscanner/fragment/DateFragment.kt
@@ -0,0 +1,47 @@
+package com.ikm.inventoryqrscanner.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.ikm.inventoryqrscanner.databinding.FragmentDateBinding
+import java.util.*
+
+class DateFragment(var listener: DateListener) : BottomSheetDialogFragment() {
+
+    private lateinit var binding: FragmentDateBinding
+    private var date: String = ""
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentDateBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupListener()
+    }
+
+    private fun setupListener() {
+        binding.calender.date = Date().time
+        binding.calender.setOnDateChangeListener { _, year, month, day ->
+            date = "$day/${month + 1}/$year"
+        }
+
+        binding.btnApply.setOnClickListener {
+            listener.onSuccess(date)
+            this.dismiss()
+        }
+    }
+
+    interface DateListener {
+        fun onSuccess(date: String)
+    }
+
+}
