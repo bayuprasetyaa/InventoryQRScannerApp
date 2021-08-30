f9eb256b7583330c0da096fcd51ab2f6caeecf34
BayuPrasetya <bayud40@gmail.com>
2021-08-30 13:56:25 +0700

    Create Activity

--- app/src/main/java/com/ikm/inventoryqrscanner/activity/HomeActivity.kt
+++ app/src/main/java/com/ikm/inventoryqrscanner/activity/HomeActivity.kt
@@ -1,12 +1,28 @@
 package com.ikm.inventoryqrscanner.activity
 
+import android.content.Intent
 import android.os.Bundle
 import com.ikm.inventoryqrscanner.BaseActivity
-import com.ikm.inventoryqrscanner.R
+import com.ikm.inventoryqrscanner.databinding.ActivityHomeBinding
 
 class HomeActivity : BaseActivity() {
+
+    private val binding by lazy { ActivityHomeBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setupBinding()
+        setupListener()
     }
+
+    private fun setupBinding() {
+        setContentView( binding.root )
+    }
+
+    private fun setupListener(){
+        binding.fabAdd.setOnClickListener {
+            startActivity(Intent(this, CreateActivity::class.java))
+        }
+    }
+
 }
