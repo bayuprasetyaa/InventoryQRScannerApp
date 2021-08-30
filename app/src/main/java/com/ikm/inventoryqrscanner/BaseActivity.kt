077325dc608ae129689ed7a9ee757711a8e84f86
BayuPrasetya <bayud40@gmail.com>
2021-08-30 11:20:25 +0700

    Add home, adapter and recycleview

---
+++ app/src/main/java/com/ikm/inventoryqrscanner/BaseActivity.kt
@@ -0,0 +1,17 @@
+package com.ikm.inventoryqrscanner
+
+import android.content.pm.ActivityInfo
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+
+open class BaseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_base)
+
+        supportActionBar!!.hide()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+    }
+}
---
+++ app/src/main/java/com/ikm/inventoryqrscanner/activity/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.ikm.inventoryqrscanner.activity
+
+import android.os.Bundle
+import com.ikm.inventoryqrscanner.BaseActivity
+import com.ikm.inventoryqrscanner.R
+
+class HomeActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
